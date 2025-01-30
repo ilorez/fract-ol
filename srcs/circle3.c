849a5abcae64cf8562e48684f209554a746559bb
ilorez <zobirnajdaoui@gmail.com>
2025-01-30 12:37:19 +0100

    working on zoom

--- srcs/circle3.c
+++ srcs/circle3.c
@@ -30,16 +30,28 @@ typedef struct	s_data {
   void *mlx;
   void *win;
   double zoom;
+  t_cor *center;
   t_img_data *img_data;
   t_cor *cor;
 }				t_data;
 
+int calculate_color(int x, int y , int color)
+{
+  return color;
+  int sum = x * y;
+  if (sum > 0)
+    return (color - sum );
+  else if (sum < 0)
+    return (color * sum * -1);
+  return (color);
+
+}
 void ft_put_pixel(t_img_data *data, t_cor *cor, int color)
 {
   char *dst;
 
   dst = data->addr + cor->y * data->ll + cor->x * (data->bpp / 8);
-  *(unsigned int*)dst = color;
+  *(unsigned int*)dst = calculate_color(cor->x, cor->y, color);
 }
 
 
@@ -67,7 +79,7 @@ void circle_fractol(t_data *data, int center_x, int center_y, int radius, int co
   draw_circle(data, center_x, center_y, radius, color);
   //mlx_clear_window(data->mlx, data->win);
   //mlx_put_image_to_window(data->mlx, data->win, data->img_data->img, 0, 0);
-  if (radius < 1 * data->zoom)
+  if (radius < data->zoom)
     return ;
   circle_fractol(data, center_x + radius, center_y + radius, radius / 2, color / 100);
   circle_fractol(data, center_x - radius, center_y - radius, radius / 2, color + 100);
@@ -77,10 +89,19 @@ void circle_fractol(t_data *data, int center_x, int center_y, int radius, int co
 
 int	key_hook(int keycode, t_data *data)
 {
+    //ft_printf("%d\n", keycode);
     if (keycode == 65362) // Up arrow
         data->zoom *= 1.1;
     else if (keycode == 65364) // Down arrow
         data->zoom /= 1.1;
+    else if (keycode == 'w') // w
+        data->center->y += 5;
+    else if (keycode == 'a') // a
+        data->center->x += 5;
+    else if (keycode == 's') // s
+        data->center->y -= 5;
+    else if (keycode == 'd') // d
+        data->center->x -= 5;
     else if (keycode == 65307) // ESC
     {
         mlx_destroy_window(data->mlx, data->win);
@@ -88,21 +109,29 @@ int	key_hook(int keycode, t_data *data)
     }
     mlx_clear_window(data->mlx, data->win);
     ft_bzero(data->img_data->addr, WIDTH * HEIGHT * data->img_data->bpp / 8);
-    circle_fractol(data, CENTER_X - 100, CENTER_Y - 100, RADIUS * data->zoom, COLOR);
+    circle_fractol(data, data->center->x, data->center->y, RADIUS * data->zoom, COLOR);
     mlx_put_image_to_window(data->mlx, data->win, data->img_data->img, 0, 0);
     return (0);
 }
 
+void get_new_zoom_center(int x, int y, t_data *data)
+{
+  data->center->x = (data->center->x - x) * data->zoom + x;
+  data->center->y = (data->center->y - y) * data->zoom + y;
+  return ;
+}
+
 int handle_mouse_click(int button, int x, int y, t_data *data)
 {
-  t_cor center;
   if (button == 4)
         data->zoom *= 1.1;
   if (button == 5)
         data->zoom /= 1.1;
-  mlx_clear_window(data->mlx, data->win);
+
+  get_new_zoom_center(x, y, data);
+  //mlx_clear_window(data->mlx, data->win);
   ft_bzero(data->img_data->addr, WIDTH * HEIGHT * data->img_data->bpp / 8);
-  circle_fractol(data, x + CENTER_X, y + CENTER_Y, RADIUS * data->zoom, COLOR);
+  circle_fractol(data, data->center->x, data->center->y, RADIUS * data->zoom, COLOR);
   mlx_put_image_to_window(data->mlx, data->win, data->img_data->img, 0, 0);
   return (0);
 }
@@ -117,10 +146,12 @@ int main()
 {
   t_data *data;
   t_cor *cor;
+  t_cor *center;
   t_img_data *img_data;
 
   data = ft_calloc(sizeof(t_data), 1);
   cor = ft_calloc(sizeof(t_cor), 1);
+  center = ft_calloc(sizeof(t_cor), 1);
   img_data = ft_calloc(sizeof(t_img_data), 1);
 
   data->mlx = mlx_init();
@@ -128,6 +159,9 @@ int main()
   img_data->img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
   img_data->addr = mlx_get_data_addr(img_data->img, &img_data->bpp, &img_data->ll, &img_data->endian);
   data->cor = cor;
+  center->x = CENTER_X;
+  center->y = CENTER_Y;
+  data->center = center;
   data->img_data = img_data;
   data->zoom = 1;
 
