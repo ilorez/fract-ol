670317b00b4cb1ab4549f0a47478feea3660a234
ilorez <zobirnajdaoui@gmail.com>
2025-01-31 11:45:25 +0100

    norm

--- srcs/fractol.c
+++ srcs/fractol.c
@@ -12,77 +12,70 @@
 
 #include "fractol.h"
 
+typedef struct s_cor
+{
+	int			x;
+	int			y;
+}				t_cor;
 
-typedef struct s_cor {
-    int x;
-    int y;
-} t_cor;
-
-typedef struct s_img_data {
-    void *img;
-    char *addr;
-    int bpp;
-    int ll;
-    int endian;
-} t_img_data;
+typedef struct s_img_data
+{
+	void		*img;
+	char		*addr;
+	int			bpp;
+	int			ll;
+	int			endian;
+}				t_img_data;
 
 typedef enum s_fractol
 {
-  F_JULIA = 1,
-  F_MANDERBROT
-} t_fractol;
+	F_JULIA = 1,
+	F_MANDERBROT
+}				t_fractol;
 
-
-typedef struct s_data {
-    void *mlx;
-    void *win;
-    double zoom;
-    t_fractol fractol;
-    t_cor *center;
-    t_img_data *img_data;
-    t_cor *cor;
-    t_errno errno;
-} t_data;
-
-
-void ft_print_syntax_usage()
+typedef struct s_data
 {
-  ft_printf("usage:\n");
-  exit(1);
+	void		*mlx;
+	void		*win;
+	double		zoom;
+	t_fractol	fractol;
+	t_cor		*center;
+	t_img_data	*img_data;
+	t_cor		*cor;
+	t_errno		errno;
+}				t_data;
+
+void	ft_print_syntax_usage(void)
+{
+	ft_printf("usage:\n");
+	exit(1);
 }
 
-t_data *fractol_init()
+t_data	*fractol_init(void)
 {
-  t_data *data;
-  data = ft_calloc(sizeof(t_data), 1);
+	t_data	*data;
 
-
-  data->mlx = 
-
+	data = ft_calloc(sizeof(t_data), 1);
+	data->mlx =
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-  t_fractol selected;
-  t_data *data;
-  if (ac < 2 || !av[1][0])
-    ft_print_syntax_usage();
-  selected = ft_get_selected(av[1]); 
-  data = ft_init_data(data, selected);
-  if (!data)
-      ft_free_and_show_error(data);
-  if (!ft_create_mlx_window(data))
-      ft_free_and_show_error(data);
-  if (!ft_draw_fractol(data))
-      return (1);
-  // get params
-  // get check if first param contain valid fractol
-  // init data using the selected fractol
+	t_fractol	selected;
+	t_data		*data;
 
-
-
-
-
-
-  return 0;
+	if (ac < 2 || !av[1][0])
+		ft_print_syntax_usage();
+	selected = ft_get_selected(av[1]);
+	data = ft_init_data(data, selected);
+	if (!data)
+		ft_free_and_show_error(data);
+	if (!ft_create_mlx_window(data))
+		ft_free_and_show_error(data);
+	if (!ft_draw_fractol(data))
+		return (1);
+	// get params
+	// get check if first param contain valid fractol
+	// init data using the selected fractol
+	return (0);
 }
--- srcs/ft_errno.c
+++ srcs/ft_errno.c
@@ -6,33 +6,32 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 10:51:57 by znajdaou          #+#    #+#             */
-/*   Updated: 2025/01/31 11:29:01 by znajdaou         ###   ########.fr       */
+/*   Updated: 2025/01/31 11:41:55 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "t_errno.h"
 
-
-static void _ft_create_err_list(t_errno_msg lst[ERR_COUNT])
+static void	_ft_create_err_list(t_errno_msg lst[ERR_COUNT])
 {
-  t_errno_msg	err_msgs[ERR_COUNT];
+	t_errno_msg	err_msgs[ERR_COUNT];
 
 	err_msgs[ERR_SUCCESS] = (t_errno_msg){ERR_SUCCESS, "OK\n"};
 	err_msgs[ERR_INVALID_ARG] = (t_errno_msg){ERR_INVALID_ARG,
 		"invalid argument\n"};
-	err_msgs[ERR_EMPTY_ARG] = (t_errno_msg){ERR_EMPTY_ARG,
-		"Empty argumnet\n"};
+	err_msgs[ERR_EMPTY_ARG] = (t_errno_msg){ERR_EMPTY_ARG, "Empty argumnet\n"};
 	err_msgs[ERR_MALLOC_FAIL] = (t_errno_msg){ERR_MALLOC_FAIL,
 		"an allocation failed\n"};
 	err_msgs[ERR_UNKNOWN] = (t_errno_msg){ERR_UNKNOWN,
 		"Unknown error occurred\n"};
-  ft_memcpy(lst, err_msgs, sizeof(t_errno_msg) * ERR_COUNT);
+	ft_memcpy(lst, err_msgs, sizeof(t_errno_msg) * ERR_COUNT);
 }
 
 void	ft_strerr(t_errno error_code)
 {
 	t_errno_msg	err_msgs[ERR_COUNT];
-  _ft_create_err_list(err_msgs);
+
+	_ft_create_err_list(err_msgs);
 	if (error_code < 0 || error_code >= ERR_COUNT)
 		error_code = ERR_UNKNOWN;
 	ft_putstr_fd(err_msgs[error_code].message, STDERR_FILENO);
@@ -41,24 +40,26 @@ void	ft_strerr(t_errno error_code)
 void	ft_perror(char *msg, t_errno error_code)
 {
 	t_errno_msg	err_msgs[ERR_COUNT];
-  _ft_create_err_list(err_msgs);
+
+	_ft_create_err_list(err_msgs);
 	if (error_code < 0 || error_code >= ERR_COUNT)
 		error_code = ERR_UNKNOWN;
-  if (!msg || !*msg)
-	  ft_putstr_fd("Error", STDERR_FILENO);
-  else
-	  ft_putstr_fd(msg, STDERR_FILENO);
+	if (!msg || !*msg)
+		ft_putstr_fd("Error", STDERR_FILENO);
+	else
+		ft_putstr_fd(msg, STDERR_FILENO);
 	ft_putstr_fd(": ", STDERR_FILENO);
 	ft_putstr_fd(err_msgs[error_code].message, STDERR_FILENO);
 }
 
+/*
 int	main(int argc, char *argv[]) {
 	// Example 1: Direct printing
 	if (argc < 2) {
 		ft_strerr(ERR_EMPTY_ARG);
 		return (1);
 	}
-  else 
-	  ft_perror(argv[1], ERR_SUCCESS);
+  else
+		ft_perror(argv[1], ERR_SUCCESS);
 	return (0);
-}
+}*/
