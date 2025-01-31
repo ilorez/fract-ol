1fa670d564ebb1e91dc87de9a6d0f09471300fca
ilorez <zobirnajdaoui@gmail.com>
2025-01-31 19:20:00 +0100

    stop in trying to hande moves using arrows and ESC using esc key

--- srcs/events.c
+++ srcs/events.c
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 17:44:16 by znajdaou          #+#    #+#             */
-/*   Updated: 2025/01/31 18:55:02 by znajdaou         ###   ########.fr       */
+/*   Updated: 2025/01/31 19:19:28 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,31 @@ int ft_handle_mouse_click(int key, int x, int y, t_data *data)
     ft_bzero(data->img_data->addr, WIDTH * HEIGHT * data->img_data->bpp / 8);
     if (!ft_draw_fractol(data))
         ft_handle_window_exit(data, ERR_UNKNOWN);
+    return 0;
+}
+
+// okay no thing checked here 
+int key_hook(int keycode, t_data *data) {
+    if (keycode == 65362) // Up arrow
+        data->zoom *= data->zoom_inc;
+    else if (keycode == 65364) // Down arrow
+        data->zoom /= data->zoom_inc;
+    else if (keycode == KEY_UP) // w
+        data->center->y += 5;
+    else if (keycode == KEY_LEFT) // a
+        data->center->x += 5;
+    else if (keycode == KEY_DOWN) // s
+        data->center->y -= 5;
+    else if (keycode == KEY_RIGHT) // d
+        data->center->x -= 5;
+    else if (keycode == KEY_ESC) // ESC 65307
+    {
+        mlx_destroy_window(data->mlx, data->win);
+        exit(0);
+    }
+    mlx_clear_window(data->mlx, data->win);
+    ft_bzero(data->img_data->addr, WIDTH * HEIGHT * data->img_data->bpp / 8);
+    circle_fractol(data, data->center->x, data->center->y, RADIUS * data->zoom, COLOR);
+    mlx_put_image_to_window(data->mlx, data->win, data->img_data->img, 0, 0);
     return 0;
 }
